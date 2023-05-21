c2ce9ce296125d42537e66d3db39290b1d272149
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-05-21 22:54:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,9 @@
-var line = document.getElementById("line");
+const lineElement = document.getElementById('line');
+let rotationAngle = 0;
 
-// Set the line's properties
-line.style.color = "black";
-line.style.width = "200px";
-line.style.height = "2px";
-
-// Create a function to rotate the line
 function rotateLine() {
-  // Get the current angle of the line
-  var angle = line.style.transform.match(/rotate\((\d+)deg\)/)[1];
-
-  // Rotate the line by 1 degree
-  angle++;
-
-  // Set the line's transform property
-  line.style.transform = "rotate(" + angle + "deg)";
+  rotationAngle += 1;
+  lineElement.style.transform = `rotate(${rotationAngle}deg)`;
 }
 
-// Start rotating the line
-setInterval(rotateLine, 100);
-//your JS code here. If required.
-var line = document.getElementById("line");
-
-// Set the line's properties
-line.style.color = "black";
-line.style.width = "200px";
-line.style.height = "2px";
-
-// Create a function to rotate the line
-function rotateLine() {
-  // Get the current angle of the line
-  var angle = line.style.transform.match(/rotate\((\d+)deg\)/)[1];
-
-  // Rotate the line by 1 degree
-  angle++;
-
-  // Set the line's transform property
-  line.style.transform = "rotate(" + angle + "deg)";
-}
-
-// Start rotating the line
-setInterval(rotateLine, 100);
+setInterval(rotateLine, 10);
